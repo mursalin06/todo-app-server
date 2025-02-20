a437fd1106940789fc8883b0f3a1be75cfa09fc5
MursalinDevOps <mursalin1768@gmail.com>
2025-02-20 22:02:53 +0600

    project deployed

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
+        // await client.connect();
+        // await client.db("admin").command({ ping: 1 });
         console.log(
             "Pinged your deployment. You successfully connected to MongoDB!"
         );
